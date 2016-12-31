6338e1f7d83e7ee3bb272dcb21b4070366932a7b
Harun GÜLGÜN <harungulgun@gmail.com>
2016-12-31 19:27:41 +0300

    UrunEklemeİslemiYapıldı
    Urunleri mysql veritabanına ekledim ve ordaki urunler tablosunu dataGrid
    e çektim

--- FinalGörsel2/harungulgun/harungulgun/urungiris.xaml.cs
+++ FinalGörsel2/harungulgun/harungulgun/urungiris.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Threading;
 
 namespace harungulgun
 {
@@ -19,11 +22,27 @@ namespace harungulgun
     /// </summary>
     public partial class urungiris : Window
     {
+        MySqlConnection bag = new MySqlConnection("Server = localhost; Database = stoktakibi; Uid = root; Pwd=;");
+        System.Windows.Threading.DispatcherTimer zaman;
         public urungiris()
         {
             InitializeComponent();
+            zaman = new System.Windows.Threading.DispatcherTimer();
+            zaman.Interval = new TimeSpan(0,0,1);
+            zaman.Tick += Zaman_Tick;
+            zaman.Start();
+        }
+        private void Zaman_Tick(object sender, EventArgs e)
+        {
+            MySqlCommand cm = new MySqlCommand("Select * from urunler",bag);
+            MySqlDataAdapter ad = new MySqlDataAdapter(cm);
+            DataTable tb = new DataTable();
+            ad.Fill(tb);
+            urungoster.ItemsSource = tb.AsDataView();
+
         }
 
+       
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
             AnaEkran ana = new AnaEkran();
@@ -33,6 +52,11 @@ namespace harungulgun
 
         private void urunekle_Click(object sender, RoutedEventArgs e)
         {
+            bag.Open();
+            MySqlCommand ekle = new MySqlCommand("INSERT INTO urunler (urunad,urunadet,ureticiFirma,barkodno,fiyati,giristarihi) VALUES ('" + urunad.Text + "','" + urunadet.Text + "','" + urunuret.Text + "','" + urunbarkod.Text + "','"+urunfiyat.Text+"','"+giristarih.Text+"')",bag);
+            ekle.ExecuteNonQuery();
+            ekle.Dispose();
+            bag.Close();
 
         }
     }
