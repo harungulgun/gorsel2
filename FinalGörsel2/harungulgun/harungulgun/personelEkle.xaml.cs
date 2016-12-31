531bf471b22017e45d28243f075417d928b1d59c
Harun GÜLGÜN <harungulgun@gmail.com>
2016-12-31 21:43:01 +0300

    Personel Ekleme
    Personel Ekleme İşlemi Yapıldı

--- FinalGörsel2/harungulgun/harungulgun/personelEkle.xaml.cs
+++ FinalGörsel2/harungulgun/harungulgun/personelEkle.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Threading;
 
 namespace harungulgun
 {
@@ -19,9 +22,27 @@ namespace harungulgun
     /// </summary>
     public partial class personelEkle : Window
     {
+        MySqlConnection bag = new MySqlConnection("Server = localhost; Database = stoktakibi; Uid = root; Pwd=;");
+        DispatcherTimer zaman;
         public personelEkle()
         {
             InitializeComponent();
+            cinsiyet.Items.Add("Erkek");
+            cinsiyet.Items.Add("Kız");
+            zaman = new System.Windows.Threading.DispatcherTimer();
+            zaman.Interval = new TimeSpan(0, 0, 1);
+            zaman.Tick += Zaman_Tick;
+            zaman.Start();
+
+        }
+        private void Zaman_Tick(object sender, EventArgs e)
+        {
+            MySqlCommand cm = new MySqlCommand("Select * from personel", bag);
+            MySqlDataAdapter ad = new MySqlDataAdapter(cm);
+            DataTable tb = new DataTable();
+            ad.Fill(tb);
+            personelgoster.ItemsSource = tb.AsDataView();
+
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
@@ -35,6 +56,15 @@ namespace harungulgun
         private void MenuItem_Click_1(object sender, RoutedEventArgs e)
         {
             
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            bag.Open();
+            MySqlCommand ekle = new MySqlCommand("INSERT INTO personel(personelTc,personelAd,personelSoyad,cinsiyet,tarih) VALUES ('" + tcno.Text + "','" + perad.Text+ "','" + persoyad.Text + "','" + cinsiyet.Text + "','" +gtarih.Text +"')", bag);
+            ekle.ExecuteNonQuery();
+            ekle.Dispose();
+            bag.Close();
         }
     }
 }
