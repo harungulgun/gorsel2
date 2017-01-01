97a9a4b48be161c09fd0eda0cf89d787f439c3e8
Harun GÜLGÜN <harungulgun@gmail.com>
2017-01-01 21:22:14 +0300

    Ürünleri Silme İşlemi Yapıldı
    Ürünler kayıt edildikten sonra silme işlemi gerçekleştirildi.

--- FinalGörsel2/harungulgun/harungulgun/urunsil.xaml.cs
+++ FinalGörsel2/harungulgun/harungulgun/urunsil.xaml.cs
@@ -23,15 +23,25 @@ namespace harungulgun
     public partial class urunsil : Window
     {
         MySqlConnection bag = new MySqlConnection("Server = localhost; Database = stoktakibi; Uid = root; Pwd=;");
-    
+        DispatcherTimer zamanaba;
         public urunsil()
         {
             InitializeComponent();
+            zamanaba = new DispatcherTimer();
+            urunlerigoster();
         }
-        private void Zamanab_Tick(object sender, EventArgs e)
+        private void urunlerigoster()
         {
-
-
+            MySqlCommand asd = new MySqlCommand("select * from urunler", bag);
+            MySqlDataAdapter ad = new MySqlDataAdapter(asd);
+            DataTable ta = new DataTable();
+            ad.Fill(ta);
+            sildata.ItemsSource = ta.AsDataView();
+        }
+        private void Zamanaba_Tick(object sender, EventArgs e)
+        {
+            urunlerigoster();
+            
 
         }
        
@@ -40,6 +50,21 @@ namespace harungulgun
             AnaEkran ana = new AnaEkran();
             ana.Show();
             this.Hide();
+        }
+
+        private void urunekle_Click(object sender, RoutedEventArgs e)
+        {
+            bag.Open();
+            DataRowView satir = (DataRowView)sildata.SelectedItem;
+            if (sildata.SelectedIndex == -1) return;
+            MySqlCommand sil = new MySqlCommand("delete from urunler where id='" + satir[0] + "'", bag);
+            MySqlDataAdapter adabtor = new MySqlDataAdapter(sil);
+            sil.ExecuteNonQuery();
+            sil.Dispose();
+            bag.Close();
+            zamanaba.Interval = new TimeSpan(0, 0, 0);
+            zamanaba.Tick += Zamanaba_Tick;
+            zamanaba.Start();
         }
     }
 }
